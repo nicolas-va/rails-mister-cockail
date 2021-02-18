614893a24b871c7fa6e44a825e150dbf06624ba0
nicolas vassel <nicolas.vassel@gmail.com>
2021-02-18 16:52:45 +0000

    association-validation done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :description, uniqueness: true
+  validates :cocktail_id, uniqueness: {scope: :ingredient_id}
+
+
 end
